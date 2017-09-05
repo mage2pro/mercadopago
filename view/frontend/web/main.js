f09a5d8835c5c0fa9fd178beaf45eeb7f79d50eb
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-09-06 02:34:00 +0300

    no message

--- view/frontend/web/main.js
+++ view/frontend/web/main.js
@@ -1,5 +1,7 @@
 // 2017-04-24
-define(['Df_StripeClone/main'], function(parent) {'use strict'; return parent.extend({
+define(['Df_StripeClone/main'], function(parent) {'use strict';
+/** 2017-09-06 @uses Class::extend() https://github.com/magento/magento2/blob/2.2.0-rc2.3/app/code/Magento/Ui/view/base/web/js/lib/core/class.js#L106-L140 */	
+return parent.extend({
 	/**
 	 * 2017-04-24
 	 * The bank card network codes: https://mage2.pro/t/2647
