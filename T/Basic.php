b2df2e255e5d1ab836d1ef0fc5a2830a1f886e01
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-09-27 18:20:47 +0300

    0.0.3: «Use of echo language construct is discouraged»: https://github.com/mage2pro/core/issues/28

--- T/Basic.php
+++ T/Basic.php
@@ -3,5 +3,5 @@ namespace Dfe\MercadoPago\T;
 // 2017-04-24
 final class Basic extends TestCase {
 	/** @test 2017-04-24 */
-	function t01() {echo df_dump([]);}
+	function t01() {print_r(df_dump([]));}
 }
