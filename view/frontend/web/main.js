1e9ada6de20e3494dfd2a00099e903a941d7b824
Dmitry Fedyuk <dfediuk@gmail.com>
2018-09-29 02:17:42 +0600

    no message

--- view/frontend/web/main.js
+++ view/frontend/web/main.js
@@ -3,8 +3,10 @@ define(['Df_StripeClone/main'], function(parent) {'use strict';
 /** 2017-09-06 @uses Class::extend() https://github.com/magento/magento2/blob/2.2.0-rc2.3/app/code/Magento/Ui/view/base/web/js/lib/core/class.js#L106-L140 */	
 return parent.extend({
 	/**
-	 * 2017-04-24
-	 * The bank card network codes: https://mage2.pro/t/2647
+	 * 2017-04-24 The bank card network codes: https://mage2.pro/t/2647
+	 * @override
+	 * @see Df_Payment/main::getCardTypes()
+	 * @used-by https://github.com/mage2pro/core/blob/3.9.12/Payment/view/frontend/web/template/card/fields.html#L4
 	 * @returns {String[]}
 	 */
 	getCardTypes: function() {return ['VI', 'MC', 'AE', 'JCB', 'DI', 'DN'];},
